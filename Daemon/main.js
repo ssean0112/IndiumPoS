49300a016ee36dfbc8fc64037f8b0a0587541523
cuperus <cuperus.kevin@hotmail.com>
2020-10-05 18:14:01 +0200

    [Daemon] Moving things arround

--- Daemon/main.js
+++ Daemon/main.js
@@ -1,22 +1,26 @@
 const Core = require('./Core.js');
 const Colors = require('./Config/Colors.js');
+const RPC = require('./RPC.js');
 
 const fastify = require('fastify');
 const io = require('socket.io')(fastify.server);
 const chalk = require('chalk');
 
-const rpc = fastify();
+const rpcFastify = fastify();
 
 // Show logo and Version
 Core.showLogo();
 
 // Starting RPC server
-rpc.listen(3000, () => {
-  Core.Log(chalk.hex(Colors.celeste), 'RPC', 'Server has started at 0.0.0.0:3000');
+rpcFastify.listen(3000, () => {
+  Core.Log(chalk.hex(Colors.skyMagenta), 'RPC', 'RPC server has started at 0.0.0.0:3000');
 });
 
 // Starting P2P server
 io.listen(3001);
+
+// Log
+Core.Log(chalk.hex(Colors.purpleNavi), 'P2P', 'P2P server has started at 0.0.0.0:3000');
 
 // On P2P
 io.on('connection', socket => {
@@ -25,10 +29,5 @@ io.on('connection', socket => {
   });
 });
 
-// On RPC 
-rpc.get("/url", (req, res, next) => {
-  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-});
-
-// Log
-Core.Log(chalk.magenta, 'P2P', 'Server has started at 0.0.0.0:3000');
+// RPC requests 
+RPC.getInfo(rpcFastify);
