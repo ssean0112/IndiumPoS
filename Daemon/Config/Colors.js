e72d592ab7829bcf3f17a6e6625c5887fc4e02e1
cuperus <cuperus.kevin@hotmail.com>
2020-10-05 17:48:17 +0200

    [Daemon] Created colors.js

---
+++ Daemon/Config/Colors.js
@@ -0,0 +1,17 @@
+let Colors = {
+    standard:       '#0081a9',
+    standardLight:  '#8ed7ed',
+
+    sizzlingRed:    '#F2545B',
+    amaranthPurple: '#A93F55',
+    purpleNavi:     '#4F518C',
+    skyMagenta:     '#C179B9',
+    bluePurple:     '#BCB6FF',
+    celadon:        '#ACF7C1',
+    keppel:         '#49BEAA',
+    mustard:        '#FCD757',
+    celeste:        '#A9FFF7',
+    
+}
+
+module.exports = Colors;
