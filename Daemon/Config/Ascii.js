6471e7f8a2102603d36d66d6195a0507ba1f20d1
cuperus <cuperus.kevin@hotmail.com>
2020-10-05 15:23:19 +0200

    [Daemon] Added ASCII logo

---
+++ Daemon/Config/Ascii.js
@@ -0,0 +1,34 @@
+var Ascii = {
+  Logo:
+    `ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss\n` +
+    `ssssssssssssssssssssssssssssssssssssssssssso+:-/ssssssssssssssssss\n` +
+    `ssssssssssssssssssssssssssssssssssssssso+:-'    -ossssssssssssssss\n` +
+    `ssssssssssssssssssssssssssssssssssso+:-'     '   ./ossssssssssssss\n` +
+    `ssssssssssssssssssssssssssssssso+:-'     '-+yy:'   -osssssssssssss\n` +
+    `sso++++++++++ooooooooooooooo+/-'     '-+ydmmdmms.   '/osssssssssss\n` +
+    `ss+.       ''''''''''''''..'     '-+ydmmhsoyy/yNd/'   .+ssssssssss\n` +
+    `sss/     ''''''''            '-/ydmmhs+::+hdd:'/dNy-   ':ossssssss\n` +
+    `ssso.   .hddhhhhhhyyyyyyssssydmmhs+/:::/shddd+  .smm/'   .+sssssss\n` +
+    `ssss+'   oNNdyyyyyyhhhhhhhhdhs+/::::::+hdddddy'  '/dNy-    :osssss\n` +
+    `sssss:   .hNmhyso+++++++++++/:::::::/shddddddh-    'smm+'   '/ssss\n` +
+    `ssssso'   /mNdhhhhyso++++++++/:::::ohddddddddh/     ':hNh:    -oss\n` +
+    `ssssss/   'sNmdhhhhhhhyso++++/:::/ydddddddddddo'      'sNN/    :ss\n` +
+    `sssssso-   .mNmhhhhhhhhhhhyso+/:ohdddddddddddds.     ':hNh.   -oss\n` +
+    `sssssss+'   oNNdhhhhhhhhhhhhhhsshhhdddddddddddh:   .:+hNd-   .osss\n` +
+    `ssssssss:   .hNmhhhhhhhhhhhhhysssssssyyyhhhhhdd/'.:/+yNm:   .+ssss\n` +
+    `sssssssso.   :mNdhhhhhhhhhhhysssssssssssssssyyhs///+yNN/   '+sssss\n` +
+    `sssssssss/'  'sNNdhhhhhhhhyssssssssssssyyhhdmmmy+/+sNm+   '/ssssss\n` +
+    `ssssssssso-   .dNmhhhhhhhyssssssssyyhhdmmmmmmmms++omNo'   :sssssss\n` +
+    `ssssssssss+'   /NNdhhhhysssssyyhhdmmmmmmmmmmmmdo/odNs'   :ssssssss\n` +
+    `sssssssssss/   'yNmhhhysyyhddmmmmmmmmmmmmmmmmmdo+dNh'   -ossssssss\n` +
+    `ssssssssssss.   :mNmhhddmmmmmmmmmmmmmmmmmmmmmmhohNh-   .osssssssss\n` +
+    `ssssssssssss+'   ommmNNNNNNNNNNmmmmmmmmmmmmmmmhhNd:   .ossssssssss\n` +
+    `sssssssssssss:   '.--:/++osyhhddmmmNNNNNNNNNNmmNm:   '+sssssssssss\n` +
+    `ssssssssssssso.'             '''.--://+ossyhddmm/   '/ssssssssssss\n` +
+    `sssssssssssssso+++//::--...''''           ''''..    /sssssssssssss\n` +
+    `ssssssssssssssssssssssssssooo+++//::--...'''       :ssssssssssssss\n` +
+    `sssssssssssssssssssssssssssssssssssssssssooo++///:/ossssssssssssss\n` +
+    `ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss`
+}
+
+module.exports = Ascii;
