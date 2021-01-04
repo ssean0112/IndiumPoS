546f25c5a17ec45ec5b7ff20201a879e50cadcca
cuperus <cuperus.kevin@hotmail.com>
2021-01-04 06:39:17 +0100

    [Daemon] Updating config file

--- Daemon/Config/Config.js
+++ Daemon/Config/Config.js
@@ -1,15 +1,20 @@
 const JsonPackage = require('../package.json');
 
-let Config = {
-    coinName:                   "Indium",
-    coinTicker:                 "IND",
-    coinNetworkId:              "1NDN3T",
-    coinNetworkVersion:         1000,
-    
-    buildVersion:               JsonPackage.version,
-    buildName:                  "BlueDragon",
+/* What are atomic units?
+ * 1 coin is equal to 100000000 atomic units */
 
-    difficultyTarget:           "60",
+let Config = {
+    coinName:                   "Indium",                   // This is the coin name
+    coinTicker:                 "IND",                      // This is the coin ticker
+    coinNetworkId:              "1NDN3T",                   // This is the coin network id
+    coinNetworkVersion:         1000,                       // This is the coin network version
+    
+    buildVersion:               JsonPackage.version,        // This is the build version
+    buildName:                  "BlueDragon",               // This is the build name
+
+    difficultyTarget:           "60",                       // This is the difficulty target, also known as the amount of seconds it takes to mine a block
+
+    premineAmount:              10000                       // Premine is specified in atomic units.
 }
 
 module.exports = Config;
