a1f3178dcfb751e51e62c4d0803d2ff0513edbf3
cuperus <cuperus.kevin@hotmail.com>
2021-01-04 06:29:35 +0100

    [Daemon] make random id

--- Daemon/Core.js
+++ Daemon/Core.js
@@ -24,6 +24,15 @@ var Core = {
     console.log(chalk.hex(Colors.standard)(Ascii.Logo) + "\n");
     console.log('    ' + chalk.hex(Colors.standardLight)(`${Config.CoinName} Daemon v${JsonPackage.version}         Copyright © 2020, ${JsonPackage.author}  \n`));
   }
+  makeRandomId: function (length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  },
 }
 
 module.exports = Core;
