b8315f2a44e0017e1eaec4335d36a17065d01c94
cuperus <cuperus.kevin@hotmail.com>
2021-01-04 07:38:08 +0100

    [Daemon] Updating Config.js

--- Daemon/Config/Config.js
+++ Daemon/Config/Config.js
@@ -1,7 +1,10 @@
 const JsonPackage = require('../package.json');
 
 /* What are atomic units?
- * 1 coin is equal to 100000000 atomic units */
+ * 1 coin is equal to 100000000 atomic units 
+ *
+ * What can I change after creating the coin?
+ * Everything marked with a * is changeable after creation of the coin */
 
 let Config = {
     coinName:                   "Indium",                   // This is the coin name
@@ -9,8 +12,8 @@ let Config = {
     coinNetworkId:              "1NDN3T",                   // This is the coin network id
     coinNetworkVersion:         1000,                       // This is the coin network version
     
-    buildVersion:               JsonPackage.version,        // This is the build version
-    buildName:                  "BlueDragon",               // This is the build name
+    buildVersion:               JsonPackage.version,        // * This is the build version
+    buildName:                  "BlueDragon",               // * This is the build name
 
     difficultyTarget:           "60",                       // This is the difficulty target, also known as the amount of seconds it takes to mine a block
     maxBlocks:                  300000000,                  // * Maximum blocks possible on the chain
