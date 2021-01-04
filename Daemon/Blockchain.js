e5d3e85e0f2093fa54477d14db79f52abe9c8f59
cuperus <cuperus.kevin@hotmail.com>
2021-01-04 08:21:35 +0100

    [Daemon] blockchain database checks

--- Daemon/Blockchain.js
+++ Daemon/Blockchain.js
@@ -19,13 +19,54 @@ let Blockchain = {
       if(block == undefined) {
         break;
       } else {
-        // Check if block difficulty is valid
+        // Check if block difficulty is a valid type
         if(isNaN(block.difficulty)) {
           console.log('');
-          Core.Danger(`Block '${i}' had not a valid difficulty\n`)
+          Core.Danger(`Block '${i}' has not a valid difficulty\n`)
           Core.Warning('Daemon stopped because of an error')
           process.exit();
         }
+
+        // Check if block version matches
+        if(isNaN(block.version)) {
+          console.log('');
+          Core.Danger(`Block '${i}' has not a valid version\n`)
+          Core.Warning('Daemon stopped because of an error')
+          process.exit();
+        } else {
+          if(block.version !== Config.coinNetworkVersion) {
+            console.log('');
+            Core.Danger(`Block '${i}' has not a matching version\n`)
+            Core.Warning('Daemon stopped because of an error')
+            process.exit();
+          }
+        }
+
+        // Check if previous block hash is a valid type
+        if(typeof block.previousBlockHash !== "string" || block.previousBlockHash.length !== 64) {
+          console.log('');
+          Core.Danger(`Block '${i}' has not a valid previous block hash type\n`)
+          Core.Warning('Daemon stopped because of an error')
+          process.exit();
+        }
+
+        // Check if merkle root is a valid type
+        if(typeof block.merkleRoot !== "string" || block.merkleRoot.length !== 64) {
+          console.log('');
+          Core.Danger(`Block '${i}' has not a valid merkle root type\n`)
+          Core.Warning('Daemon stopped because of an error')
+          process.exit();
+        }
+
+        // Check if timestamp is a valid type
+        if(isNaN(block.timestamp)) {
+          console.log('');
+          Core.Danger(`Block '${i}' has not a valid timestamp\n`)
+          Core.Warning('Daemon stopped because of an error')
+          process.exit();
+        }
+
+        // Set current height
         Core.currentSyncHeight = i;
       }
     }
